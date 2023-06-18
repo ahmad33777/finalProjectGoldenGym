deb6a4f3753805c1f0c413b38c8e67abb33c7cdd
Suhaib200119 <sis.22.18a@gmail.com>
2023-06-18 15:42:45 +0300

    push FacebookController

---
+++ app/Http/Controllers/FacebookController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Illuminate\Http\Request;
+
+class FacebookController extends Controller
+{
+
+    public function create()
+    {
+        return view('facebook.createPost');
+
+    }
+
+    public function createPost(Request $request)
+    {
+        $token = "#";
+        $app_id = '#';
+        $app_secret = '#';
+
+        $fb = new Facebook([
+            'app_id' => $app_id,
+            'app_secret' => $app_secret,
+            'default_graph_version' => 'v3.3',
+        ]);
+
+        $fb->setDefaultAccessToken($token);
+        $post = [
+            'message' => $request->post_bady,
+        ];
+        $response = $fb->post('/me/feed', $post);
+        $graphNode = $response->getGraphNode();
+        if ($graphNode) {
+            session()->flash('status', true);
+            return redirect()->route('create-post');
+        } else {
+            session()->flash('status', false);
+            return redirect()->route('create-post');
+
+        }
+    }
+
+
+}
